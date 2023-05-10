dd7a6a9d170721481ce67aa9c6866b9292409e70
srillu <saishri1148@gmail.com>
2023-05-10 11:57:44 +0000

    solution

--- src/components/BookmarkRoute/index.js
+++ src/components/BookmarkRoute/index.js
@@ -1,14 +1,22 @@
 import BookmarkItem from '../BookmarkItem'
 import BookmarkContext from '../../context/BookmarkContext'
 
+import './index.css'
+
 const BookmarkRoute = () => (
   <BookmarkContext.Consumer>
     {value => {
       const {bookmarkList} = value
 
-      console.log(bookmarkList)
+      console.log(bookmarkList.length)
 
-      return (
+      const noBookItems = bookmarkList.length === 0
+
+      return noBookItems ? (
+        <div className="no-bookmarks">
+          <h1>No BookMarks To Show </h1>
+        </div>
+      ) : (
         <ul>
           <h1>My Bookmarks</h1>
           {bookmarkList.map(eachItem => (
